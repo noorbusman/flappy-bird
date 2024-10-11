ccd7573618bfefef5fffb23974b6ae34bdb2889a
noorbusman <125867335+noorbusman@users.noreply.github.com>
2024-10-11 14:29:19 +0200

    Update flappy_bird.py
    added def run (config_path) and if name __name__ = "main"

--- flappy_bird.py
+++ flappy_bird.py
@@ -387,3 +387,20 @@ def main(win):
     end_screen(WIN)
 
 main(WIN)
+
+def run(config_path): 
+    config = neat.config.Config(neat.DefaultGenome, neat.DefaultReproduction, 
+                                neat.DefaultSpeciesSet, neat.DefaultStagnation, 
+                                config_path)
+    p = neat.Population(config)
+
+    p.add_repoter(neat.StdOutReporter(True))
+    stats = neat.StatisticsReporter()
+    p.add_reporter(stats) 
+
+    winner = p.run(,50) 
+
+if __name__ == "main__":
+    local_dir = os.pass.dirname(__file__)
+    config_path = os.path.join(local_dir, "config-feedforward.txt")
+    run(config_path) 
