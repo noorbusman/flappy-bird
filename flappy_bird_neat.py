5b8e489e4010932b8606f016b8c7e6909255af47
noorbusman <125867335+noorbusman@users.noreply.github.com>
2024-10-14 20:27:45 +0200

    Update flappy_bird_neat.py

--- flappy_bird_neat.py
+++ flappy_bird_neat.py
@@ -261,46 +261,46 @@ class Base:
 
        surf.blit(rotated_image, new_rect.topleft)
 
-   def draw_window(win, birds, pipes, base, score, gen, pipe_ind):
-      """
-      draws the windows for the main game loop
-      :param win: pygame window surface
-      :param bird: a Bird object
-      :param pipes: List of pipes
-      :param score: score of the game (int)    	    	                
-      :param gen: current generation
-      :param pipe_ind: index of closest pipe
-      """
-       if gen == 0:
-          gen = 1
-         win.blit(bg_img, (0,0))
+    def draw_window(win, birds, pipes, base, score, gen, pipe_ind):
+       """
+       draws the windows for the main game loop
+       :param win: pygame window surface
+       :param bird: a Bird object
+       :param pipes: List of pipes
+       :param score: score of the game (int)    	    	                
+       :param gen: current generation
+       :param pipe_ind: index of closest pipe
+       """
+        if gen == 0:
+           gen = 1
+          win.blit(bg_img, (0,0))
 
-    for pipe in pipes:
-        pipe.draw(win)
+     for pipe in pipes:
+         pipe.draw(win)
 
-    base.draw(win)
-    for bird in birds:
-        # draw lines from bird to pipe
-        if DRAW_LINES:
-            try:
-                pygame.draw.line(win, (255,0,0), (bird.x+bird.img.get_width()/2, bird.y + bird.img.get_height()/2), (pipes[pipe_ind].x + pipes[pipe_ind].PIPE_TOP.get_width()/2, pipes[pipe_ind].height), 5)
-                pygame.draw.line(win, (255,0,0), (bird.x+bird.img.get_width()/2, bird.y + bird.img.get_height()/2), (pipes[pipe_ind].x + pipes[pipe_ind].PIPE_BOTTOM.get_width()/2, pipes[pipe_ind].bottom), 5)
-            except:
-                pass
-        # draw bird
-        bird.draw(win)
+     base.draw(win)
+     for bird in birds:
+         # draw lines from bird to pipe
+         if DRAW_LINES:
+             try:
+                 pygame.draw.line(win, (255,0,0), (bird.x+bird.img.get_width()/2, bird.y + bird.img.get_height()/2), (pipes[pipe_ind].x + pipes[pipe_ind].PIPE_TOP.get_width()/2, pipes[pipe_ind].height), 5)
+                 pygame.draw.line(win, (255,0,0), (bird.x+bird.img.get_width()/2, bird.y + bird.img.get_height()/2), (pipes[pipe_ind].x + pipes[pipe_ind].PIPE_BOTTOM.get_width()/2, pipes[pipe_ind].bottom), 5)
+             except:
+                 pass
+         # teken bird
+         bird.draw(win)
 
-    # score
-    score_label = STAT_FONT.render("Score: " + str(score),1,(255,255,255))
-    win.blit(score_label, (WIN_WIDTH - score_label.get_width() - 15, 10))
+     # score
+     score_label = STAT_FONT.render("Score: " + str(score),1,(255,255,255))
+     win.blit(score_label, (WIN_WIDTH - score_label.get_width() - 15, 10))
 
-    # generations
-    score_label = STAT_FONT.render("Gens: " + str(gen-1),1,(255,255,255))
-    win.blit(score_label, (10, 10))
+     # generations
+     score_label = STAT_FONT.render("Gens: " + str(gen-1),1,(255,255,255))
+     win.blit(score_label, (10, 10))
 
-    # alive
-    score_label = STAT_FONT.render("Alive: " + str(len(birds)),1,(255,255,255))
-    win.blit(score_label, (10, 50))
+     # alive
+     score_label = STAT_FONT.render("Alive: " + str(len(birds)),1,(255,255,255))
+     win.blit(score_label, (10, 50))
 
     pygame.display.update()
 
